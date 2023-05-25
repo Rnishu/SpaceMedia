bf276f287d46c84209878f1c5fc5f7613b9f7041
yashkanjariyaa <133478846+yashkanjariyaa@users.noreply.github.com>
2023-05-25 15:38:57 +0530

    Add files via upload

--- homepage.js
+++ homepage.js
@@ -1,48 +1,10 @@
-function const_view(planet){
-    const planet_text = document.getElementByCLass("planet_div_text");
-    planet_text[planet].style.opacity="100%";
-    }
-function const_disappear(planet){
-    const planet_text = document.getELementByClass("planet_div_text");
-    planet_text[planet].style.opacity="0%"
-}
-function view(num){
-  if(num == 1){
-        document.getElementById("planet_1_div_text").style.animation="fade 2s ease-out 0.5s";
-        setTimeout(const_view(num),2000);
-  }else if(num == 2){
-        document.getElementById("planet_2_div_text").style.animation="fade 2s ease-out 0.5s";
-        setTimeout(const_view(num),2000);
-  }else if(num == 3){
-        document.getElementById("planet_3_div_text").style.animation="fade 2s ease-out 0.5s";
-        setTimeout(const_view(num),2000);
-  }else if(num == 4){
-        document.getElementById("planet_4_div_text").style.animation="fade 2s ease-out 0.5s";
-        setTimeout(const_view(num),2000);
-  }
-}
-function disappear(num){
-  if(num == 1){
-        document.getElementById("planet_1_div_text").style.animation="fade 2s ease-out 0.5s reverse";
-        setTimeout(const_disappear(num),2000);
-  }else if(num == 2){
-        document.getElementById("planet_2_div_text").style.animation="fade 2s ease-out 0.5s reverse";
-        setTimeout(const_disappear(num),2000);
-  }else if(num == 3){
-        document.getElementById("planet_3_div_text").style.animation="fade 2s ease-out 0.5s reverse";
-        setTimeout(const_disappear(num),2000);
-  }else if(num == 4){
-        document.getElementById("planet_4_div_text").style.animation="fade 2s ease-out 0.5s reverse";
-        setTimeout(const_disappear(num),2000);
-  }
-}
-const planet_text = document.getElementsByClassName("planet_div_text");
-function const_view(planet){
-    planet_text[planet].style.opacity="100%";
-}
-for(let i=0;i<planet_text.length;i++){
-    setTimeout(const_view,4000,i);
-}
+/*function removes and adds show class according to section 
+becoming visible on screen.
+IntersectionObserver api looks out for elements that intersect
+with root element i.e.; viewport or any other element. When elements
+interact this class can be used to call a function to manipulate.
+Enteries is an array of elements that intersect. Code below targets
+the sections with class="iHidden"*/
 const observer = new IntersectionObserver((enteries) => {
       setInterval(
       enteries.forEach((entry) => {
@@ -54,8 +16,5 @@ const observer = new IntersectionObserver((enteries) => {
                   }
       }),2000);
   });
-  const hiddenSection = document.querySelectorAll('.iHidden');
-  hiddenSection.forEach((el) => observer.observe(el));
-function loginPage(){
-      document.querySelectorAll('.section').style.display="none";
-}
+const hiddenSection = document.querySelectorAll('.iHidden');
+hiddenSection.forEach((el) => observer.observe(el));
