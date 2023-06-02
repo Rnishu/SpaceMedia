5a90b1892811ab19865f801af589d1cebd85eec6
yashkanjariyaa <133478846+yashkanjariyaa@users.noreply.github.com>
2023-06-02 08:04:52 +0530

    Update ss_homepage.js
    added comments, slowed down rotations of planets

--- ss_homepage.js
+++ ss_homepage.js
@@ -1,7 +1,17 @@
+/*The program that runs the solar system animation
+we start with targetting all planets using 'planet' class
+then we make a array. We manually enter an array for radius and 
+velocity. setInterval is used to run fucntion repeatively on 10 millisecond 
+interval. Top and left properties are altered in small time. Values given is 
+product of radius and cosine fucntion of radian at the instant for left(x) and
+product of radius and sine fucntion of radian at the instant ofr top (y). This
+would actually giver circular revolution, but multiply the left component with 2.
+then according to the position of revolution, z index is changed to make the planet
+go behind the sun while rotating.*/ 
 const planets = document.querySelectorAll('.planet');
-const p_radii = [35,40,50,70,112,138,165,190];
+const p_radii = [20,40,20,50,70,38,65,50];
 let p_radians = new Array(8).fill(0);
-const p_velocities = [0.1, 0.05,0.1,0.8, 0.434, 0.323, 0.228, 0.182];
+const p_velocities = [0.1, 0.05,0.1,0.1, 0.07, 0.05, 0.1, 0.09];
 
 
 setInterval( ()=> {
